d0881f9e5491cb4b6f59049d7ab0dca25bff9e77
Komari Herring <komherr@gmail.com>
2016-09-08 12:33:56 -0400

    Coding Kata Week 1

---
+++ LeapYear/FoodSpoilage.m
@@ -0,0 +1,42 @@
+//  FoodSpoilage
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+    @autoreleasepool
+    
+    {
+        
+        
+        
+        int baconAge = 6;
+        int baconSpoilage = 7;
+        int eggAge = 19;
+        int eggSpoilage = 21;
+        
+        NSLog(@"The bacon is %d days old.", baconAge);
+        
+        NSLog(@"The eggs are %d days old.", eggAge);
+        
+        
+        
+        
+        if (baconAge <= baconSpoilage && eggAge <= eggSpoilage)
+        {
+            NSLog(@"The bacon and eggs are good to eat.");
+        }
+        if (baconAge > baconSpoilage)
+        {
+            NSLog(@"Throw away the bacon!");
+        }
+        if (eggAge > eggSpoilage)
+        {
+            NSLog(@"Throw away the eggs!");
+        }
+        
+        
+    }
+    
+    return 0;
+}
